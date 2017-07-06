d117c11f8524577db5c859d4a23e4ecd9fc9d444
Bo Lingen <lingenbw@gmail.com>
2017-07-06 18:36:12 +0000

    test: improve and fix `if-not-ci` tests

--- tests/index.js
+++ tests/index.js
@@ -7,12 +7,16 @@ const ifNotCI = resolve(__dirname, '..', 'bin', 'if-not-ci.js')
 const cwd = resolve(__dirname, 'fixtures')
 const args = ['npm', 'run', 'hello']
 
+function parseOutput (stdout) {
+  const lines = stdout.split(/\r?\n/g).filter(Boolean)
+  return lines[lines.length - 1]
+}
+
 test('if-ci: runs the given command when in a CI environment', async t => {
   const env = { CI: true }
   const result = await execa(ifCI, args, { cwd, env })
-  const lines = result.stdout.split(/\r?\n/g)
-
-  t.is(lines[lines.length - 1], 'yo what up')
+  const stdout = parseOutput(result.stdout)
+  t.is(stdout, 'it works!')
 })
 
 test('if-ci: does not run the given command when not in a CI environment', async t => {
@@ -21,10 +25,13 @@ test('if-ci: does not run the given command when not in a CI environment', async
 })
 
 test('if-not-ci: runs the given command when not in a CI environment', async t => {
-  const result = await execa(ifNotCI, args, { cwd, extendEnv: false })
-  const lines = result.stdout.split(/\r?\n/g)
+  const { env } = process
+  ;['CI', 'CONTINUOUS_INTEGRATION', 'BUILD_NUMBER', 'TRAVIS']
+    .forEach(name => delete env[name])
 
-  t.is(lines[lines.length - 1], 'yo what up')
+  const result = await execa(ifNotCI, args, { cwd })
+  const stdout = parseOutput(result.stdout)
+  t.is(stdout, 'it works!')
 })
 
 test('if-not-ci: does not run the given command when in a CI environment', async t => {
